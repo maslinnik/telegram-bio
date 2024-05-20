5170eb6e1c67c0dd276f756fcfec3b7c221b2233
maslinnik <pervutinskiy.ri@gmail.com>
2024-05-20 17:51:59 +0300

    Add signal handler to updater script

---
+++ update_all.py
@@ -0,0 +1,33 @@
+from app import db
+from app.models import Username
+from update_user import update_users
+import signal
+from threading import Event
+
+
+exit = Event()
+
+
+def main():
+    while not exit.is_set():
+        print('Starting to update')
+        try:
+            users = db.session.query(Username).filter(Username.monitored == True).all()
+            update_users(users)
+        except Exception as e:
+            print('Following exception occurred:')
+            print(e)
+        else:
+            print(f'Updated {len(users)} users')
+        exit.wait(60)
+
+
+def quit(*_):
+    print("Request to shutdown received, stopping")
+    exit.set()
+
+
+signal.signal(signal.SIGINT, quit)
+signal.signal(signal.SIGTERM, quit)
+
+main()
