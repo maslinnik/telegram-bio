fc9f58ac064ecb4b2a1b55298899707612d469cd
maslinnik <pervutinskiy.ri@gmail.com>
2024-05-20 16:01:10 +0300

    Add web scraping option to APIManager

--- api_manager.py
+++ api_manager.py
@@ -3,10 +3,61 @@ from config import Config
 from telethon import TelegramClient
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import User
+from abc import ABC, abstractmethod
 import asyncio
+from bs4 import BeautifulSoup
+import requests
 
 
-class APIManager:
+class APIManager(ABC):
+    @abstractmethod
+    def user_exists_batch(self, usernames: Iterable[str]) -> Iterable[bool]:
+        pass
+
+    @abstractmethod
+    def user_exists(self, username: str) -> bool:
+        pass
+
+    @abstractmethod
+    def get_bio_batch(self, usernames: Iterable[str]) -> Iterable[str]:
+        pass
+
+    @abstractmethod
+    def get_bio(self, username: str) -> str:
+        pass
+
+
+class WebScraper(APIManager):
+    def __get_tgme_soup(self, username: str) -> BeautifulSoup:
+        r = requests.get(f'https://t.me/{username}')
+        return BeautifulSoup(r.text, features="html.parser")
+
+    def user_exists(self, username: str) -> bool:
+        soup = self.__get_tgme_soup(username)
+        name_obj = soup.find('div', {'class': 'tgme_page_title'})
+        preview_obj = soup.find('div', {'class': 'tgme_page_context_link_wrap'})
+        return name_obj is not None and preview_obj is None
+
+    def get_bio(self, username: str) -> str:
+        soup = self.__get_tgme_soup(username)
+        name_obj = soup.find('div', {'class': 'tgme_page_title'})
+        preview_obj = soup.find('div', {'class': 'tgme_page_context_link_wrap'})
+        bio_obj = soup.find('div', {'class': 'tgme_page_description'})
+        if name_obj is None or preview_obj is not None or bio_obj is None:
+            return ''
+        delimiter = '\0'
+        for line_break in bio_obj.findAll('br'):  # loop through line break tags
+            line_break.replaceWith(delimiter)
+        return bio_obj.text.replace(delimiter, '\n')
+
+    def user_exists_batch(self, usernames: Iterable[str]) -> Iterable[bool]:
+        return [self.user_exists(username) for username in usernames]
+
+    def get_bio_batch(self, usernames: Iterable[str]) -> Iterable[str]:
+        return [self.get_bio(username) for username in usernames]
+
+
+class MTProtoAPIManager(APIManager):
     def user_exists_batch(self, usernames: Iterable[str]) -> Iterable[bool]:
         async def main():
             client = await (TelegramClient('session', Config.TG_API_ID, Config.TG_API_HASH)
